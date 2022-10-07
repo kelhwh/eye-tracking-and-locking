97c55e529925f73da79ee42da0256188e2706d68
Frank Hsieh <frank1571@gmail.com>
2022-10-07 22:37:28 +0800

    show pupil location on image

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import mediapipe as mp
 import json
+from util import add_text
 
 with open('landmarks.json') as file:
     landmarks = json.load(file)
@@ -17,7 +18,7 @@ cap = cv.VideoCapture(0)
 
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
-    refine_landmarks=True,
+    refine_landmarks=True, #Turn on iris and lip mesh points
     min_detection_confidence=0.9,
     min_tracking_confidence=0.9
 ) as face_mesh:
@@ -26,7 +27,7 @@ with mp_face_mesh.FaceMesh(
         ret, frame = cap.read()
         if not ret:
             break
-        frame = cv.flip(frame, 1) #Flip vertically for webcam mirrored image
+        frame = cv.flip(frame, 1) #Flip vertically to show webcam mirrored image
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         img_h, img_w = frame.shape[:2]
         faces = face_mesh.process(frame)
@@ -40,6 +41,16 @@ with mp_face_mesh.FaceMesh(
             cv.circle(frame, mesh_points[LEFT_PUPIL], radius=1, color=(0, 0, 255), thickness=-1)
             cv.circle(frame, mesh_points[RIGHT_PUPIL], np.linalg.norm(mesh_points[RIGHT_PUPIL] - mesh_points[RIGHT_IRIS][0]).astype(int), (0, 0, 255), thickness=1, lineType=cv.LINE_AA)
             cv.circle(frame, mesh_points[RIGHT_PUPIL], radius=1, color=(0, 0, 255), thickness=-1)
+
+            text = f"Left pupil: {mesh_points[LEFT_PUPIL]} \nRight pupil: {mesh_points[RIGHT_PUPIL]}"
+            add_text(
+                frame,
+                text,
+                org=(int(img_w*0.05), int(img_h*0.1)),
+                font=cv.FONT_HERSHEY_COMPLEX,
+                font_scale=0.5,
+                color=(0,255,0)
+            )
 
         cv.imshow('img', frame)
         key = cv.waitKey(1)
