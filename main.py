4150c7ece839350ce7234dceaa3bdcd9fe36920d
kelhwh <frank1571@gmail.com>
2022-10-10 15:19:33 +0800

    add comments

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from util import add_text
 with open('config/landmarks.json') as file:
     landmarks = json.load(file)
 
+#Read mesh points from json
 LEFT_IRIS = landmarks['iris']['left']
 LEFT_PUPIL = landmarks['pupil']['left']
 RIGHT_IRIS = landmarks['iris']['right']
 RIGHT_PUPIL = landmarks['pupil']['right']
 
+#The diameter for the central pupil locking area as the percentage of the iris diameter
 LOCK_SIZE = 0.5
 
 mp_face_mesh = mp.solutions.face_mesh
-
 cap = cv.VideoCapture(0)
 
 with mp_face_mesh.FaceMesh(
@@ -34,11 +35,12 @@ with mp_face_mesh.FaceMesh(
         img_h, img_w = frame.shape[:2]
         faces = face_mesh.process(frame)
 
-        if faces.multi_face_landmarks:
+        if faces.multi_face_landmarks: #If face detected in the frame
             landmark = faces.multi_face_landmarks[0].landmark
             # mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in landmark])
             mesh_points = np.array([[int(p.x * img_w),int(p.y * img_h)] for p in landmark])
 
+            #Draw both irises and create detection status
             cv.circle(frame, mesh_points[LEFT_PUPIL], np.linalg.norm(mesh_points[LEFT_PUPIL] - mesh_points[LEFT_IRIS][0]).astype(int), (255, 0, 255), thickness=1, lineType=cv.LINE_AA)
             cv.circle(frame, mesh_points[LEFT_PUPIL], radius=1, color=(255, 0, 255), thickness=-1)
             cv.circle(frame, mesh_points[RIGHT_PUPIL], np.linalg.norm(mesh_points[RIGHT_PUPIL] - mesh_points[RIGHT_IRIS][0]).astype(int), (255, 0, 255), thickness=1, lineType=cv.LINE_AA)
@@ -50,6 +52,7 @@ with mp_face_mesh.FaceMesh(
             text_detect = f"[Detection]\nLeft eye: Not detected\nRight eye: Not detected"
             lock_color = (0,255,0)
 
+        #Draw locking range and create locking status
         if 'lock_left_pupil' in globals():
             deviation_left = np.linalg.norm(mesh_points[LEFT_PUPIL] - lock_left_pupil).round(1)
             is_deviated_left = deviation_left > lock_left_diameter
@@ -63,11 +66,10 @@ with mp_face_mesh.FaceMesh(
             cv.circle(frame, lock_left_pupil, lock_left_diameter, lock_color, thickness=1, lineType=cv.LINE_AA)
             cv.circle(frame, lock_left_pupil, radius=1, color=lock_color, thickness=-1)
 
-
-
         else:
             text_lock = f"\n Left eye: Not locked  Deviation: Not locked"
 
+        #Show text for status
         add_text(
             frame,
             text_detect,
@@ -86,14 +88,16 @@ with mp_face_mesh.FaceMesh(
         )
 
         cv.imshow('img', frame)
+
+        #Reading keyboard input
         key = cv.waitKey(1)
-        if key == ord('q'):
+        if key == ord('q'): #q for quit
             break
-        elif key == ord('l'):
+        elif key == ord('l'):#l for locking left eye
             if faces.multi_face_landmarks:
                 lock_left_pupil = mesh_points[LEFT_PUPIL]
                 lock_left_diameter = int(np.linalg.norm(mesh_points[LEFT_PUPIL] - mesh_points[LEFT_IRIS][0]) * LOCK_SIZE)
-        elif key == 12:
+        elif key == 12: #crtl+l for unlocking left eye
             try:
                 del lock_left_pupil
                 del lock_left_diameter
